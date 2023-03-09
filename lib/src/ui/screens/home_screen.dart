480eeda3c9063d0a0eb8add88a7deac93de88c28
ForwardOoh <soa9719@naver.com>
2023-03-10 01:12:08 +0900

    feat: 카테고리 그리드뷰 적용

--- lib/src/ui/screens/home_screen.dart
+++ lib/src/ui/screens/home_screen.dart
@@ -3,11 +3,62 @@ import '../theme/app_color.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:for_in_trip/src/bloc/home_bloc.dart';
 
+class Category {
+  final String name;
+  final String image;
+
+  Category({required this.name, required this.image});
+}
+
+List<Category> categories = [
+  Category(name: 'Category 1', image: 'images/bd.jpg'),
+  Category(name: 'Category 2', image: 'images/bz.jpg'),
+  Category(name: 'Category 3', image: 'images/bz.jpg'),
+  Category(name: 'Category 4', image: 'images/ep.jpg'),
+  Category(name: 'Category 5', image: 'images/ep.jpg'),
+  Category(name: 'Category 6', image: 'images/bd.jpg'),
+  Category(name: 'Category 7', image: 'images/gw.jpg'),
+  Category(name: 'Category 8', image: 'images/gw.jpg'),
+  Category(name: 'Category 1', image: 'images/bd.jpg'),
+  Category(name: 'Category 2', image: 'images/bz.jpg'),
+  Category(name: 'Category 3', image: 'images/bz.jpg'),
+  Category(name: 'Category 4', image: 'images/ep.jpg'),
+  Category(name: 'Category 5', image: 'images/ep.jpg'),
+  Category(name: 'Category 6', image: 'images/bd.jpg'),
+  Category(name: 'Category 7', image: 'images/gw.jpg'),
+  Category(name: 'Category 8', image: 'images/gw.jpg'),
+  Category(name: 'Category 1', image: 'images/bd.jpg'),
+  Category(name: 'Category 2', image: 'images/bz.jpg'),
+  Category(name: 'Category 3', image: 'images/bz.jpg'),
+  Category(name: 'Category 4', image: 'images/ep.jpg'),
+  Category(name: 'Category 5', image: 'images/ep.jpg'),
+  Category(name: 'Category 6', image: 'images/bd.jpg'),
+  Category(name: 'Category 7', image: 'images/gw.jpg'),
+  Category(name: 'Category 8', image: 'images/gw.jpg'),
+  Category(name: 'Category 1', image: 'images/bd.jpg'),
+  Category(name: 'Category 2', image: 'images/bz.jpg'),
+  Category(name: 'Category 3', image: 'images/bz.jpg'),
+  Category(name: 'Category 4', image: 'images/ep.jpg'),
+  Category(name: 'Category 5', image: 'images/ep.jpg'),
+  Category(name: 'Category 6', image: 'images/bd.jpg'),
+  Category(name: 'Category 7', image: 'images/gw.jpg'),
+  Category(name: 'Category 8', image: 'images/gw.jpg'),
+];
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const categoryRowItemCount = 6;
+    const categoryItemSpacing = 10.0;
+    final mediaSize = MediaQuery.of(context).size;
+    final double itemSize = ((mediaSize.width -
+                30.0 -
+                (categoryItemSpacing * (categoryRowItemCount - 1))) /
+            categoryRowItemCount) -
+        20.0;
+
     final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
 
     return Scaffold(
@@ -51,36 +102,47 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(15.0),
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              BlocBuilder<CounterBloc, int>(
-                builder: (context, count) {
-                  return Text(
-                    '$count',
-                    style: TextStyle(fontSize: 48.0),
+              // BlocBuilder<CounterBloc, int>(
+              //   builder: (context, count) {
+              //     return Text(
+              //       '$count',
+              //       style: TextStyle(fontSize: 48.0),
+              //     );
+              //   },
+              // ),
+              // SizedBox(height: 24.0),
+              GridView.builder(
+                itemCount: (categories.length <= 24 ? categories.length : 24),
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 6,
+                  crossAxisSpacing: categoryItemSpacing,
+                  mainAxisSpacing: categoryItemSpacing,
+                ),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        height: itemSize,
+                        width: itemSize,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(categories[index].image),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      SizedBox(height: 2.0),
+                      Text(
+                        categories[index].name,
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                    ],
                   );
                 },
-              ),
-              SizedBox(height: 24.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  FloatingActionButton(
-                    onPressed: () {
-                      counterBloc.add(CounterEvent.decrement);
-                    },
-                    tooltip: 'Decrement',
-                    child: Icon(Icons.remove),
-                  ),
-                  SizedBox(width: 24.0),
-                  FloatingActionButton(
-                    onPressed: () {
-                      counterBloc.add(CounterEvent.increment);
-                    },
-                    tooltip: 'Increment',
-                    child: Icon(Icons.add),
-                  ),
-                ],
               ),
             ],
           ),
