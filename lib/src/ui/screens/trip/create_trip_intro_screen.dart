c66c0803efe612b4c44652f137b27583d186ec40
ForwardOoh <soa9719@naver.com>
2023-04-01 00:20:22 +0900

    feat: 여행 인트로 추가

--- lib/src/ui/screens/trip/create_trip_intro_screen.dart
+++ lib/src/ui/screens/trip/create_trip_intro_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:for_in_trip/src/ui/widgets/appbar/menu_appbar.dart';
+import 'package:for_in_trip/src/ui/theme/app_color.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
 class CreateTripIntroScreen extends StatefulWidget {
   @override
   _CreateTripIntroScreenState createState() => _CreateTripIntroScreenState();
@@ -8,31 +12,42 @@ class CreateTripIntroScreen extends StatefulWidget {
 class _CreateTripIntroScreenState extends State<CreateTripIntroScreen> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController _tripNameController = TextEditingController();
-  DateTime _startDate = DateTime.now();
-  DateTime _endDate = DateTime.now();
+  DateTimeRange? _dateRange;
 
-  void _selectStartDate(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
-        context: context,
-        initialDate: _startDate,
-        firstDate: DateTime.now(),
-        lastDate: DateTime(2100));
-    if (pickedDate != null && pickedDate != _startDate) {
+  Future<void> _selectDateRange(BuildContext context) async {
+    final initialDateRange = DateTimeRange(
+      start: DateTime.now(),
+      end: DateTime.now().add(Duration(days: 1)),
+    );
+    final pickedDateRange = await showModalBottomSheet<DateTimeRange>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 300,
+          child: SfDateRangePicker(
+            selectionMode: DateRangePickerSelectionMode.range,
+            initialSelectedRange: PickerDateRange(
+              initialDateRange.start,
+              initialDateRange.end,
+            ),
+            onSelectionChanged: (DateRangePickerSelectionChangedArgs args) {
+              setState(() {
+                _dateRange = DateTimeRange(
+                  start: args.value.startDate!,
+                  end: args.value.endDate!,
+                );
+              });
+            },
+          ),
+        );
+      },
+    );
+    if (pickedDateRange != null) {
       setState(() {
-        _startDate = pickedDate;
-      });
-    }
-  }
-
-  void _selectEndDate(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
-        context: context,
-        initialDate: _endDate,
-        firstDate: _startDate,
-        lastDate: DateTime(2100));
-    if (pickedDate != null && pickedDate != _endDate) {
-      setState(() {
-        _endDate = pickedDate;
+        _dateRange = DateTimeRange(
+          start: pickedDateRange.start,
+          end: pickedDateRange.end,
+        );
       });
     }
   }
@@ -40,9 +55,7 @@ class _CreateTripIntroScreenState extends State<CreateTripIntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Trip Intro'),
-      ),
+      appBar: MenuAppBar(title: "Create Trip"),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
@@ -50,6 +63,41 @@ class _CreateTripIntroScreenState extends State<CreateTripIntroScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20.0, horizontal: 8.0),
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.black,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Make you a trip! ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      // TextSpan(
+                      //   text: 'For In Trip!\n',
+                      //   style: TextStyle(
+                      //     fontWeight: FontWeight.bold,
+                      //     color: primaryColor,
+                      //   ),
+                      // ),
+                      TextSpan(
+                        text: 'Please enter information about your trip!',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 26.0,
+                          color: gray1,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               TextFormField(
                 controller: _tripNameController,
                 decoration: InputDecoration(labelText: 'Trip Name'),
@@ -62,45 +110,69 @@ class _CreateTripIntroScreenState extends State<CreateTripIntroScreen> {
               ),
               SizedBox(height: 16.0),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Start Date: '),
-                  GestureDetector(
-                    onTap: () {
-                      _selectStartDate(context);
-                    },
-                    child: Text(
-                      '${_startDate.toLocal()}'.split(' ')[0],
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
+                  Text(
+                    'Travel Start: ',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 16),
+                  Icon(Icons.arrow_right),
+                  SizedBox(width: 16),
+                  Text(
+                    'Travel End: ',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                 ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _selectDateRange(context);
+                },
+                child: Text('Select Date Range'),
               ),
               SizedBox(height: 16.0),
-              Row(
-                children: [
-                  Text('End Date: '),
-                  GestureDetector(
-                    onTap: () {
-                      _selectEndDate(context);
-                    },
-                    child: Text(
-                      '${_endDate.toLocal()}'.split(' ')[0],
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              if (_dateRange != null)
+                Text(
+                  'Selected Date Range: ${_dateRange!.start.toLocal()} - ${_dateRange!.end.toLocal()}',
+                  style: TextStyle(fontSize: 16),
+                ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Container(
+                      height: 48.0,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate() &&
+                              _dateRange != null) {
+                            // Form is valid and date range is selected, do something with the data
+                            String tripName = _tripNameController.text;
+                            DateTime startDate = _dateRange!.start;
+                            DateTime endDate = _dateRange!.end;
+
+                            // TODO: Do something with the data
+
+                            // Navigate back to previous screen
+                            Navigator.pop(context);
+                          }
+                        },
+                        child: Text(
+                          "Submit",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange,
+                        ),
+                      ),
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 32.0),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      // Submit form logic here
-                    }
-                  },
-                  child: Text('Submit'),
                 ),
               ),
             ],
